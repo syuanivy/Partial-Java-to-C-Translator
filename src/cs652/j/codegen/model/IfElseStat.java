a05af2ff4ee277231deddac474377fbb4f5fb7c3
syuanivy <syuanivy@gmail.com>
2015-04-04 16:36:42 -0700

    ifElse @Model Element bug fixed, ifElse passed

--- src/cs652/j/codegen/model/IfElseStat.java
+++ src/cs652/j/codegen/model/IfElseStat.java
@@ -4,7 +4,7 @@ package cs652.j.codegen.model;
  * Created by Shuai on 3/27/15.
  */
 public class IfElseStat extends IfStat {
-    public Stat elseStatement;
+    @ModelElement public Stat elseStatement;
 
     public IfElseStat() {
         super();
